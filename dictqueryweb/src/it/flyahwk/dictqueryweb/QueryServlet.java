8448ca20dee4d237035e9f01c7783a54b5293285
liuyonglong <lyl2725@gmail.com>
2019-05-04 22:34:33 +0800

    使用DeclarativeService的方式（component文件)来实现服务的注册和引用

--- dictqueryweb/src/it/flyahwk/dictqueryweb/QueryServlet.java
+++ dictqueryweb/src/it/flyahwk/dictqueryweb/QueryServlet.java
@@ -8,42 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
 import it.flyhawk.dictquery.DictQueryService;
 
 public class QueryServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-
-	private BundleContext context;
-
-	private ServiceTracker m_tracker = null;
-
-	public QueryServlet(BundleContext context) {
-		this.context = context;
-	}
-
-	public void init() throws ServletException {
-		try {
-			m_tracker = new ServiceTracker(context, context.createFilter("(&(objectClass=" + DictQueryService.class.getName() + ")" + "(queryType=*))"), null);
-			m_tracker.open();
-		} catch (InvalidSyntaxException e) {
-			e.printStackTrace();
-		}
-	}
+	private HttpService httpService; 
+	private DictQueryService dictQueryService;
+	private static final String SERVLET_ALIAS = "/query.do";
+	private static final String STATIC_ALIAS = "/";
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("QueryServlet: starting processing the httpRequest.");
 		String word = req.getParameter("word");
 		String result = "";
 
-		// 当有多个可用服务时，随机选择一个
-		DictQueryService dictQueryService = (DictQueryService)m_tracker.getService();
 		if(dictQueryService!=null) {
 			// 当没有服务可用时，返回为null，因此需要判断是否为空。
 			result = dictQueryService.queryWord(word);
@@ -60,8 +43,46 @@ public class QueryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
 	}
+	
+	public void setHttpService(HttpService httpService) {
+		this.httpService = httpService;
+		registerWebResources();
+	}
+	
+	public void unsetHttpService(HttpService httpService) {
+		if( this.httpService != httpService) {
+			return;
+		}
+		unregisterWebResources();
+		this.httpService = null;
+	}
+	
+	public void setDictQueryService(DictQueryService dictQueryService) {
+		this.dictQueryService = dictQueryService;
+	}
+	
+	public void unsetDictQueryService(DictQueryService dictQueryService) {
+		if(this.dictQueryService != dictQueryService) {
+			return;
+		}
+		this.dictQueryService = null;
+	}
+	
+	private void registerWebResources() {
+		HttpContext httpContext = httpService.createDefaultHttpContext();
+		try {
+			// 注册静态资源
+			httpService.registerResources(STATIC_ALIAS, "/resources", httpContext);
+			// 注册servlet
+			httpService.registerServlet(SERVLET_ALIAS, this, null, httpContext);
+		
+		} catch (NamespaceException | ServletException e) {
+			e.printStackTrace();
+		}
+	}
 
-	public void destroy() {
-		m_tracker.close();
+	private void unregisterWebResources() {
+		httpService.unregister(STATIC_ALIAS);
+		httpService.unregister(SERVLET_ALIAS);
 	}
 }
